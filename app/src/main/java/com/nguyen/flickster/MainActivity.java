8f71d9e5105fb2ccb65327f4bff41513289783fd
iceguyvn <nguyen_my@yahoo.com>
2016-10-11 22:02:30 -0700

    added pull-to-refresh with SwipeRefreshLayout

--- app/src/main/java/com/nguyen/flickster/MainActivity.java
+++ app/src/main/java/com/nguyen/flickster/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nguyen.flickster;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
    List<Movie> mMovies;
    MovieArrayAdapter mAdapter;
    ListView mListView;
+   SwipeRefreshLayout mSwipeContainer;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +36,24 @@ public class MainActivity extends AppCompatActivity {
       int orientation = getResources().getConfiguration().orientation;
       mAdapter = new MovieArrayAdapter(this, mMovies, orientation);
       mListView.setAdapter(mAdapter);
+
+      fetchMovies();
+
+      mSwipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+      mSwipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+         @Override
+         public void onRefresh() {
+            fetchMovies();
+         }
+      });
+      // Configure the refreshing colors
+      mSwipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+            android.R.color.holo_green_light,
+            android.R.color.holo_orange_light,
+            android.R.color.holo_red_light);
+   }
+
+   private void fetchMovies() {
       String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
       AsyncHttpClient client = new AsyncHttpClient();
       client.get(url, new JsonHttpResponseHandler() {
@@ -42,9 +62,13 @@ public class MainActivity extends AppCompatActivity {
             JSONArray movieJsonResults = null;
             try {
                movieJsonResults = response.getJSONArray("results");
-               mMovies.addAll(Movie.fromJSONArray(movieJsonResults));
-               mAdapter.notifyDataSetChanged();
+               // clear out old items before appending new ones
+               mAdapter.clear();
+               // add new items to the adapter
+               mAdapter.addAll(Movie.fromJSONArray(movieJsonResults));
                Log.d("TRUONG", mMovies.toString());
+               // signal that refresh has finished
+               mSwipeContainer.setRefreshing(false);
             } catch (JSONException e) {
                e.printStackTrace();
             }
